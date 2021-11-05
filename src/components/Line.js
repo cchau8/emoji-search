a591fbae7a4f633520b9ece363e83f3190a02b4b
Cedric Chau <cedricchau87@gmail.com>
2021-11-05 11:22:16 +0100

    added click to copy

--- src/components/Line.js
+++ src/components/Line.js
@@ -1,9 +1,24 @@
 import React from "react";
-
+import { useState } from "react";
 const Line = ({ title, symbol }) => {
+	const [display, setDisplay] = useState(false);
 	return (
 		<li>
-			<span>{symbol}</span> <span>{title}</span>
+			<button
+				onClick={() => {
+					navigator.clipboard.writeText(symbol);
+					setDisplay(true);
+				}}
+			>
+				<div className="emoji">
+					<span>{symbol}</span> <span>{title}</span>
+				</div>
+				{display ? (
+					<span>Copied to your clipboard !</span>
+				) : (
+					<span className="click-hover">Click to copy !</span>
+				)}
+			</button>
 		</li>
 	);
 };
